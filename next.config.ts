14b79cce48e97979ec86cc5b22587ed020d37b7e
JongHyun Ryu <willowise@naver.com>
2026-03-14 18:22:36 +0900

    feat: 마크다운 문법 추가, 기본 turbopack에서 webpack 설정 빌드 시 fallback으로 추가 - Next.js에서 .md 파일을 raw 문자열로 import 할수록 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {
+    rules: {
+      "*.md": {
+        loaders: ["raw-loader"],
+        as: "*.js",
+      },
+    },
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      type: "asset/source",
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
